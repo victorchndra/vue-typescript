148150d258ef890e25b9a09103381960cbfd1e89
Victor Chandra <victorchandra0911@gmail.com>
2024-09-02 08:02:40 +0700

    final

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="app">
-    <JobsList :jobs="jobs" />
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent,  ref } from 'vue';
 import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 import JobsList from './components/JobsList.vue';
 
 export default defineComponent({
@@ -15,7 +27,6 @@ export default defineComponent({
     JobsList
   },
   setup() {
-
     const jobs = ref<Job[]>([
       { title: 'Farm Worker', location: 'Lon Lon Ranch', salary: 30000, id: '1' },
       { title: 'Quarryman', location: 'Death Mountain', salary: 40000, id: '2' },
@@ -24,12 +35,49 @@ export default defineComponent({
       { title: 'Prison Guard', location: 'Gerudo Valley', salary: 32000, id: '5' },
     ])
 
-    return { jobs }
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   },
   methods: {}
 });
 </script>
 
 <style>
+header {
+  text-align: center;
+}
 
+header .order {
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+header .title {
+  display: flex;
+  justify-content: center;
+}
+
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+
+header h1 {
+  font-size: 3em;
+}
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,18 +1,87 @@
 <template>
-  <div>
-    
+  <div class="job-list">
+    <p>Ordered by {{ order }}</p>
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <img src="@/assets/rupee.svg" alt="rupee icon">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur temporibus dolor est excepturi quas nobis quam, id accusamus maxime quasi exercitationem vero cumque, quae illum quia molestias totam odio fugiat.</p>
+        </div>
+      </li>
+    </transition-group>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import Job from '@/types/Job';
+import OrderTerm from '@/types/OrderTerm';
+import { computed, defineComponent, PropType } from 'vue'
 
 export default defineComponent({
-  props: ['jobs']
-  
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
+
+    return { orderedJobs }
+  }
 })
 </script>
 
-<style>
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
 
+.job-list ul {
+  padding: 0;
+}
+
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+
+.salary {
+  display: flex;
+}
+
+.salary img {
+  width: 30px;
+}
+
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+
+.list-move {
+  transition: all 1s;
+}
 </style>
